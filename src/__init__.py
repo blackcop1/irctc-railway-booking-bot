f268c07b0f23ca27e8494468fc2d3a59d08f9bcf
TUSHAR SUDARSHEE <tushar.24mei10102@vitbhopal.ac.in>
2026-05-07 17:37:56 +0530

    Add package initialization

---
+++ src/__init__.py
@@ -0,0 +1,7 @@
+"""IRCTC Railway Booking Automation Bot"""
+
+__version__ = '1.0.0'
+__author__ = 'blackcop1'
+__email__ = 'tushar.24mei10102@vitbhopal.ac.in'
+
+__all__ = ['core', 'browser', 'gui', 'database', 'utils']
